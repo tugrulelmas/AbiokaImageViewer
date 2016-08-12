24cf2346699aa41f2ea66d7fb5a3807cf7dc13fe
tugrul <tugrulelmas@gmail.com>
2016-08-12 11:03:22 +0300

    resizing image

--- core.js
+++ core.js
@@ -1,18 +1,53 @@
 var prevHref = "";
 var popupId = getId();
 var imgId = getId();
-var popup = "<div id='" + popupId + "' class='abioka-messagepop'><img id='" + imgId + "'</img></div>"
+var popup = "<div id='" + popupId + "' class='abioka-messagepop'><img id='" + imgId + "'></img></div>"
 $( "body" ).append(popup);
 
 document.addEventListener('mousemove', function (e) {
   var srcElement = e.srcElement;
+  if(srcElement.id === imgId)
+    return;
 
   if (srcElement.nodeName == 'A' && (srcElement.hostname == "imgur.com" || srcElement.hostname == "i.imgur.com" || srcElement.hostname == "hizliresim.com" || srcElement.hostname == "i.hizliresim.com" )) {
     if(prevHref !== srcElement.href){
-      $('#' + imgId).attr('src', 'http://littlethingsapi.abioka.com/api/imageviewer?url=' + srcElement.href);
-      $('#' + popupId).css('left', event.pageX);
-      $('#' + popupId).css('top', event.pageY);
-      $('#' + popupId).css('display','inline');
+      var mouseHeight = event.pageY;
+      var mouseWidth = event.pageX;
+      var bodyHeight = $(window).scrollTop() + $(window).height();
+      var bodyWidth = $(window).width();
+      $('#' + imgId)
+        .attr('src', 'http://littlethingsapi.abioka.com/api/imageviewer?url=' + srcElement.href)
+        .one("load", function() {
+          $('#' + popupId).css('left', mouseWidth);
+          $('#' + popupId).css('top', mouseHeight);
+          $('#' + popupId).css('display','inline');
+
+          var bottom = bodyHeight - mouseHeight;
+          var top = mouseHeight;
+
+          var imageHeight = $('#' + imgId).height();
+          if(imageHeight > bottom && bodyHeight > imageHeight){
+            top = bodyHeight - imageHeight;
+            if(imageHeight + 20 > bottom){
+              top -= 20;
+            }
+          }
+
+          var right = bodyWidth - mouseWidth;
+          var left = mouseWidth;
+
+          var imageWidth = $('#' + imgId).width();
+          if(imageWidth > right && bodyWidth > imageWidth){
+            left = bodyWidth - imageWidth;
+            if(imageWidth + 20 > right){
+              left -= 20;
+            }
+          }
+
+          $('#' + popupId).css('left', left);
+          $('#' + popupId).css('top', top);
+          $('#' + popupId).css('display','inline');
+        });
     }
 
     prevHref = srcElement.href;
