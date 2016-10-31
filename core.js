4cf45c2350f2192ea21ee43863348ba9f92b9f0e
tugrul <tugrulelmas@gmail.com>
2016-10-31 11:21:31 +0300

    removed setting src attribute

--- core.js
+++ core.js
@@ -5,7 +5,6 @@ var imgId = getId();
 var popup = "<div id='" + popupId + "' class='abioka-messagepop'><img id='" + imgId + "'></img></div>";
 var loadingUrl = chrome.extension.getURL('loading.gif');
 popup += "<div id='loading_" + popupId + "' class='abioka-messagepop abioka-messagepop-loading'><img src='" + loadingUrl + "'></img></div>";
-popup += "<img style='display:none' id='temp_" + imgId + "'></img>";
 appendToBody(popup);
 
 var validHosts = ["imgur.com", "hizliresim.com", "pbs.twimg.com", "postimg.org", "tinypic.com"];
@@ -13,128 +12,155 @@ var baseUrl = "https://stream-viper.hyperdev.space/?url=";
 var img = document.getElementById(imgId);
 var isError = false;
 
-document.addEventListener('mousemove', function (e) {
-  var srcElement = e.srcElement;
-  if(srcElement.id === imgId)
-    return;
+document.addEventListener('mousemove', function(e) {
+    var srcElement = e.srcElement;
+    if (srcElement.id === imgId)
+        return;
 
-  if (srcElement.nodeName == 'A' && (validHosts.indexOf(srcElement.hostname) > -1 || validHosts.indexOf(srcElement.hostname.split('.').slice(-2).join('.')) > -1)) {
-    if(prevHref !== srcElement.href){
-      isError = false;
-      var mouseHeight = event.pageY;
-      var mouseWidth = event.pageX;
+    if (srcElement.nodeName !== 'A' || (validHosts.indexOf(srcElement.hostname) === -1 && validHosts.indexOf(srcElement.hostname.split('.').slice(-2).join('.')) === -1)) {
+        prevHref = "";
+        closePopup();
+        return;
+    }
 
-      css(imgId, 'max-width', 'none');
-      css(imgId, 'max-height', 'none');
+    if (prevHref !== srcElement.href) {
+        isError = false;
+        var mouseHeight = event.pageY;
+        var mouseWidth = event.pageX;
 
-      setTimeout(function(){
-        if(isVisible || isError)
-          return;
+        css(imgId, 'max-width', 'none');
+        css(imgId, 'max-height', 'none');
 
-        css('loading_' + popupId, 'left', mouseWidth + "px");
-        css('loading_' + popupId, 'top', mouseHeight + "px");
-        css('loading_' + popupId, 'display','inline');
-      }, 1000);
+        setTimeout(function() {
+            if (isVisible || isError)
+                return;
 
-      var url = getUrl(srcElement.hostname, srcElement.href, srcElement.pathname);
-      img.src = baseUrl + url;
-      img.addEventListener('load', function() {
-          var bodyHeight = window.scrollY + document.documentElement.clientHeight;
-          var windowHeight = document.documentElement.clientHeight;
-          var bodyWidth = document.documentElement.clientWidth;
+            css('loading_' + popupId, 'left', mouseWidth + "px");
+            css('loading_' + popupId, 'top', mouseHeight + "px");
+            css('loading_' + popupId, 'display', 'inline');
+        }, 1000);
 
-          css(popupId, 'left', mouseWidth + "px");
-          css(popupId, 'top', mouseHeight + "px");
-          css(popupId, 'display','inline');
-
-          var bottom = bodyHeight - mouseHeight;
-          var top = mouseHeight;
-
-          var imageHeight = img.offsetHeight;
-          if(imageHeight > bottom){
-            if(windowHeight > imageHeight) {
-              top = bodyHeight - imageHeight;
-              if(imageHeight + 20 > bottom){
-                top -= 20;
-              }
-            } else{
-              top = window.scrollY + 20;
-              css(imgId, 'max-height', windowHeight - 40 + "px");
-            }
-          }
-
-          var right = bodyWidth - mouseWidth;
-          var left = mouseWidth;
-
-          var imageWidth = img.offsetWidth;
-          if(imageWidth > right){
-            if(bodyWidth > imageWidth) {
-              left = bodyWidth - imageWidth;
-              if(imageWidth + 20 > right){
-                left -= 20;
-              }
-            } else{
-              left = 20;
-              css(imgId, 'max-width', bodyWidth - 40 + "px");
-            }
-          }
-
-          css(popupId, 'left', left + "px");
-          css(popupId, 'top', top + "px");
-          css(popupId, 'display','inline');
-          css('loading_' + popupId, 'display','none');
-          isVisible = true;
-        }, false);
+        var url = getUrl(srcElement.hostname, srcElement.href, srcElement.pathname);
+        loadImage(baseUrl + url, mouseHeight, mouseWidth);
     }
 
     prevHref = srcElement.href;
-  } else {
-    prevHref = "";
-    closePopup();
-  }
 }, false);
 
-// check the hyperdev is available now.
-var tempImg = document.getElementById('temp_' + imgId);
-tempImg.src = "https://cdn.hyperdev.com/c5a3c253-8ef5-4f27-bac0-6b98c09569f9%2Fsample.jpg";
-tempImg.addEventListener('error', function() {
-  // hyperdev is not available now.
-  // use abioka server
-  baseUrl = "http://littlethingsapi.abioka.com/api/imageviewer?url=";
-}, false);
+function loadImage(url, mouseHeight, mouseWidth) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function(e) {
+        img.src = window.URL.createObjectURL(this.response);
+        img.addEventListener('load', function() {
+            setImagePosition(mouseWidth, mouseHeight);
+        }, false);
+    };
 
-img.addEventListener('error', function() {
-  closePopup();
-  isError = true;
-}, false);
+    xhr.send();
+}
 
-function closePopup(){
-  css(popupId, 'display','none');
-  css('loading_' + popupId, 'display','none');
-  isVisible = false;
+function setImagePosition(mouseWidth, mouseHeight) {
+    var bodyHeight = window.scrollY + document.documentElement.clientHeight;
+    var windowHeight = document.documentElement.clientHeight;
+    var bodyWidth = document.documentElement.clientWidth;
+
+    css(popupId, 'left', mouseWidth + "px");
+    css(popupId, 'top', mouseHeight + "px");
+    css(popupId, 'display', 'inline');
+
+    var bottom = bodyHeight - mouseHeight;
+    var top = mouseHeight;
+
+    var imageHeight = img.offsetHeight;
+    if (imageHeight > bottom) {
+        if (windowHeight > imageHeight) {
+            top = bodyHeight - imageHeight;
+            if (imageHeight + 20 > bottom) {
+                top -= 20;
+            }
+        } else {
+            top = window.scrollY + 20;
+            css(imgId, 'max-height', windowHeight - 40 + "px");
+        }
+    }
+
+    var right = bodyWidth - mouseWidth;
+    var left = mouseWidth;
+
+    var imageWidth = img.offsetWidth;
+    if (imageWidth > right) {
+        if (bodyWidth > imageWidth) {
+            left = bodyWidth - imageWidth;
+            if (imageWidth + 20 > right) {
+                left -= 20;
+            }
+        } else {
+            left = 20;
+            css(imgId, 'max-width', bodyWidth - 40 + "px");
+        }
+    }
+
+    css(popupId, 'left', left + "px");
+    css(popupId, 'top', top + "px");
+    css(popupId, 'display', 'inline');
+    css('loading_' + popupId, 'display', 'none');
+    isVisible = true;
+}
+
+function loadTempImage() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://cdn.hyperdev.com/c5a3c253-8ef5-4f27-bac0-6b98c09569f9%2Fsample.jpg", true);
+    xhr.responseType = 'blob';
+    xhr.onerror = function(e) {
+        // hyperdev is not available now.
+        // use abioka server
+        baseUrl = "http://littlethingsapi.abioka.com/api/imageviewer?url=";
+    };
+
+    xhr.send();
+}
+
+function closePopup() {
+    css(popupId, 'display', 'none');
+    css('loading_' + popupId, 'display', 'none');
+    isVisible = false;
 };
 
-function appendToBody(html){
-  document.body.insertAdjacentHTML('beforeend', html);
+function appendToBody(html) {
+    document.body.insertAdjacentHTML('beforeend', html);
 }
 
-function css(id, key, value){
-  document.getElementById(id).style[key] = value;
+function css(id, key, value) {
+    document.getElementById(id).style[key] = value;
 }
 
-function getId(){
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-  });
+function getId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
-function getUrl(hostname, fullUrl, pathName){
-  var result = fullUrl;
-  if(hostname === "imgur.com"){
-    result = "http://i.imgur.com/" + pathName.substr(pathName.indexOf('gallery/') + 'gallery/'.length) + ".png";
-  } else if(hostname === "hizliresim.com"){
-    result = "http://i.hizliresim.com/" + pathName + ".png";
-  }
-  return result;
+function getUrl(hostname, fullUrl, pathName) {
+    var result = fullUrl;
+    if (hostname === "imgur.com") {
+        result = "http://i.imgur.com/" + pathName.substr(pathName.indexOf('gallery/') + 'gallery/'.length) + ".png";
+    } else if (hostname === "hizliresim.com") {
+        result = "http://i.hizliresim.com/" + pathName + ".png";
+    }
+    return result;
 }
+
+function init() {
+    loadTempImage();
+
+    img.addEventListener('error', function() {
+        closePopup();
+        isError = true;
+    }, false);
+}
+
+init();
